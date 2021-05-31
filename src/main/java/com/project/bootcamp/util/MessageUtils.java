da45d520e8552519c749050326375612bc446eaf
Denner <dennermpb@hotmail.com>
2021-05-30 23:03:42 -0300

    Integração com postgres finalizado, POST,PUT, DELETE, etc

---
+++ src/main/java/com/project/bootcamp/util/MessageUtils.java
@@ -0,0 +1,9 @@
+package com.project.bootcamp.util;
+
+public class MessageUtils {
+
+    public static final String STOCK_ALREADY_EXISTS = "Stock already exists";
+
+    public static final String NOT_RECORDS_FOUND = "No records found";
+
+}
